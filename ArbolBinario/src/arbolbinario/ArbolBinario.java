3f83188f4076933b4a65ce37b4530ba56b5f7a78
brayanpasa99 <brayanpasa99@hotmail.com>
2020-05-12 21:19:37 -0500

    Agregar programa

---
+++ ArbolBinario/src/arbolbinario/ArbolBinario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arbolbinario;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author braya
+ */
+public class ArbolBinario extends JFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { 
+        Interfaz2 ventana = new Interfaz2();
+        ventana.setVisible(true);
+    }
+    
+}
